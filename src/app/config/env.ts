5a5acc8fcb7abcfc3adebfbe4e851d8140902c90
coderamit01 <amitmohammad46@gmail.com>
2026-07-08 11:46:56 +0600

    setup env config

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,10 +1,36 @@
+import { config } from "dotenv"
+import { AppError } from "../helpers/appError"
 
+interface EnvInterface {
+  PORT: string,
+  DATABASE_URL: string,
+  BETTER_AUTH_SECRET: string,
+  BETTER_AUTH_URL: string,
+}
 
-const loadEnv = () => {
+config()
+
+const loadEnv = ():EnvInterface => {
   const envs = [
     "PORT",
     "DATABASE_URL",
     "BETTER_AUTH_SECRET",
     "BETTER_AUTH_URL",
   ]
+
+  envs.forEach((env) => {
+    if(!process.env[env]) {
+      throw new AppError(`Missing the env file ${env}`, 404)
+    }
+  })
+
+    return {
+      PORT: process.env.PORT as string,
+      DATABASE_URL: process.env.DATABASE_URL as string,
+      BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+      BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    }
+
 }
+
+export const envVars = loadEnv();
